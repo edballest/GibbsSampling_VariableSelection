9ebfcb5d2247beef4546ce05b6f970068ae94dde
Eduardo <edballestg@gmail.com>
2018-04-21 12:35:35 -0500

    functions and loop finished. Results are wrong

--- Simulated Examples/Example4_1.R
+++ Simulated Examples/Example4_1.R
@@ -16,25 +16,10 @@ data['y'] <- mapply(FUN = function(a,b) a+1.2*b+rnorm(1,mean = 0, sd = 2.5),data
 fitP1 <- lm('y ~ X1 + X2 + X3 + X4 + X5',data)
 
 #discuss results in report
-summary(fitP1)
+t <- summary(fitP1)
 
-#Parameters
-#For this problem
-f_gamma <- function(g) (.5)^5
-tau <- 1/3
-ci <- 10
-R <- diag(rep(1,6)) #Identity
-u_gamma <- 0
 
-#always
-beta_0 <- summary(fitP1)$coefficients[,1]
-sigma_0 <- summary(fitP1)$coefficients[,2]
-gamma_0 <- rep(1,6)
-
-#functions and precomputations
-X = as.matrix(data[X_vars])
-XtX = t(X) %*% X #precompute now to avoid computing it every time
-
+#Functions
 set_a <- function(g,ci){
   #given vegtor of gammas retins vector of a
   l <- length(g)
@@ -46,20 +31,86 @@ set_a <- function(g,ci){
   }
   return(a)
 }
-
 get_inv_D <- function (g,ci){
   # returns D^-1 given vector gamma
   a <- set_a(g,ci)
   d <- 1/(tau*a) #diagonal elements
   return(diag(d))
 }
-
-get_A <- function(sigma_j,XtX,inv_D,R){
-  A <- solve((sigma_j^-2)*XtX + inv_D %*% solve(R) %*% inv_D)
+get_A <- function(sigma_jm1,XtX,inv_D_jm1,R_jm1){
+  A <- solve(sigma_jm1^-2*XtX + solve(inv_D_jm1) %*% solve(R_jm1) %*% solve(inv_D_jm1))
   return(A)
 }
 
-get_A(sigma_0,XtX,get_inv_D(gamma_0,ci),R)
+library(mvtnorm)
+library(invgamma)
+library(Rlab)
+
+get_beta_j <- function(XtX,beta_LS_est,gamma_jm1,sigma_jm1,ci,R_jm1) { #(13)
+  A_jm1 <- get_A(sigma_jm1,XtX,get_inv_D(gamma_jm1,ci),R_jm1)
+  return(rmvnorm(1, mean = (A_jm1*(sigma_jm1)^-2)%*%XtX%*%beta_LS_est,sigma = A_jm1)) 
+}
+get_sigma_j <- function(n,u_gamma_jm1,lambda_gamma_jm1,s_res) rinvgamma(1,(n+u_gamma_jm1)/2,(s_res+u_gamma_jm1*lambda_gamma_jm1)/2) #(14)
+get_gamma_j <- function(n,ci,tau,pi) {
+  gamma_j <- gamma_jm1
+  for (i in 1:n){
+    a = rnorm(1,ci*tau)*pi #(2) with gamma i = 1
+    b = rnorm(1,tau)*(1-pi) #(2) with gamma i = 0 ##ask!
+    p_bern = a/(a+b)
+    if (p_bern>1){  #does this make sense??? ##ask!
+      p_bern <- 1
+    } else if (p_bern<0){
+      p_bern <- 0
+    }
+    gamma_j[i] = rbern(1 , p_bern) #(16)
+  }
+  return(gamma_j)
+}
+
+
+#Parameters
+#For this problem
+pi <- 1/2 # fgamma follows priopr (7) and is equal to (1/2)^5
+tau <- 1/3
+ci <- 10
+R <- diag(rep(1,6)) #Identity
+u_gamma <- 0
+lambda_gamma <- 1 #anything
+
+#always
+beta_0 <- summary(fitP1)$coefficients[,1]
+sigma_0 <- summary(fitP1)$sigma
+gamma_0 <- rep(1,length(beta_0))
+
+#functions and precomputations
+X <- as.matrix(data[X_vars])
+y <- as.matrix(data$y)
+XtX <- t(X) %*% X #precompute now to avoid computing it every time
+beta_LS_est <-beta_0 #cte?  ##ask!
+
+
+#step 0
+gamma_jm1 <- gamma_0
+sigma_jm1 <- sigma_0
+results <- data.frame(combination=paste(gamma_0, collapse = ' '),stringsAsFactors = FALSE)
+
+m <- 5000
+
+for (i in 1:m){
+  #step
+  beta_j <- get_beta_j(XtX,beta_LS_est,gamma_jm1,sigma_jm1,ci,R)
+  s_res <- mean((y -X %*% t(beta_j))^2) # CHECK THIS!!!! ##ask!
+  sigma_j <- get_sigma_j(length(beta_j),u_gamma,lambda_gamma,s_res)
+  gamma_j <- get_gamma_j(length(beta_j),ci,tau,pi)
+  results[nrow(results) + 1,] = c(paste(gamma_j, collapse = ' '))
+  
+  #
+  sigma_jm1 <- sigma_j
+  gamma_jm1 <- gamma_j
+}
+
+results$combination <- as.factor(results$combination)
+table(results)
 
 
 #Problem 2
